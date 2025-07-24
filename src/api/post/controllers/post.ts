deb1c35c1582f2b89cd1acefaccb70cce88472b7
Thai Ly <thai.ly@aurora-tech.com>
2025-07-24 14:27:03 +0700

    Enable debug mode in dev script, update UID field configuration, and simplify post controller logic

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -1,51 +1,19 @@
-import { factories } from '@strapi/strapi'
+import {factories} from '@strapi/strapi';
 
-export default factories.createCoreController('api::post.post', ({ strapi }) => ({
-  // Custom controller to find post by slug
-  async findBySlug(ctx) {
-    const { slug } = ctx.params
-    const { populate } = ctx.query
+export default factories.createCoreController('api::post.post', ({strapi}) => ({
+    async findBySlug(ctx) {
+        const {slug} = ctx.params;
+        
+        const entity = await strapi.db.query('api::post.post').findOne({
+            where: {slug},
+            populate: ['featuredImage', 'author', 'categories']
+        });
 
-    const entity = await strapi.db.query('api::post.post').findOne({
-      where: { 
-        slug,
-        publishedAt: { $notNull: true }
-      },
-      populate: populate || ['featuredImage', 'author', 'categories']
-    })
+        if (!entity) {
+            return ctx.notFound();
+        }
 
-    if (!entity) {
-      return ctx.notFound('Post not found')
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+        return this.transformResponse(sanitizedEntity);
     }
-
-    // Increment view count
-    await strapi.db.query('api::post.post').update({
-      where: { id: entity.id },
-      data: { viewCount: entity.viewCount + 1 }
-    })
-
-    const sanitizedEntity = await this.sanitizeOutput(entity, ctx)
-    return this.transformResponse(sanitizedEntity)
-  },
-
-  // Override default find to only show published posts
-  async find(ctx) {
-    ctx.query.filters = {
-        ...((ctx.query.filters as object) || {}),
-      publishedAt: { $notNull: true }
-    }
-
-    const { data, meta } = await super.find(ctx)
-    return { data, meta }
-  },
-
-  // Custom method to get all slugs for static generation
-  async getSlugs(_ctx) {
-    const entities = await strapi.db.query('api::post.post').findMany({
-      select: ['slug'],
-      where: { publishedAt: { $notNull: true } }
-    })
-
-    return entities.map(entity => entity.slug)
-  }
-}))
+}));
